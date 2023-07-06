2c6cbcfab3cea678931193166227497d4d306ef1
manoranjanjetty <manoranjanjetty@gmail.com>
2023-07-06 17:26:42 +0100

    fsf

--- src/faternity.java
+++ src/faternity.java
@@ -8,21 +8,27 @@ public class faternity {
         System.out.println("Please enter the age and Gender");
         age = input.nextInt();
         gender = input.next().charAt(0);
-        switch(gender){
-            case 'm':
-            case 'M':
-            case 'f':
-            case 'F':
-                if (age >= 19){
-                    System.out.println("YOU'RE ELIGIBLE TO JOIN THE FATERNITY!!!!");
-                }//end if
-                else {
-                    System.out.println("YOU'RE NOT ELIGIBLE TO JOIN THE FATERNITY!!!!");
-                }
-                break;
-            default:
-                System.out.println("Oooops Please enter the correct gender!!!");
-        }//end of switch
 
+        if (age >= 19 && gender == 'M')
+        {
+            System.out.println("you're eligible!!!");
+        }
+        else {
+            switch (gender) {
+                case 'm':
+                case 'M':
+                case 'f':
+                case 'F':
+                    if (age >= 19) {
+                        System.out.println("YOU'RE ELIGIBLE TO JOIN THE FATERNITY!!!!");
+                    }//end if
+                    else {
+                        System.out.println("YOU'RE NOT ELIGIBLE TO JOIN THE FATERNITY!!!!");
+                    }
+                    break;
+                default:
+                    System.out.println("Oooops Please enter the correct gender!!!");
+            }//end of switch
+        }//end of else19
     }//end main
 }
