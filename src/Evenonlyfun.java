90c486da4a3144cfd7b68d28240296f8304a7e13
manoranjanjetty <manoranjanjetty@gmail.com>
2023-08-02 16:05:42 +0100

    New Change

--- src/Evenonlyfun.java
+++ src/Evenonlyfun.java
@@ -1,17 +1,24 @@
 public class Evenonlyfun {
     public static void main(String[] args){
         int input =0;
+        int count = 0;
       //  System.out.print(input +"\t");
         while (input <= 200){
             if (input % 2 != 0){
                 input++;
+                count++;
                 continue;
             }
-            System.out.print(input +"\t");
-            input++;
+            if(input == 24 || input ==50 ||input ==76 || input == 100) {
+                System.out.println(input);
+                input++;}
+            else{
+                System.out.print(input + "\t");
+                input++;
+            }
         }
-
-
+        System.out.println("\n");
+        System.out.println(count);
 
 
     }//end main
